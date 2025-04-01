f1d823dbbc51d1b3bbdcd79d8b350fb05125e3b3
Gmrgan <115547191+Gmrgan@users.noreply.github.com>
2025-04-01 14:31:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -41,6 +41,29 @@ from flask import render_template
 def show_commits():
     return render_template('commits.html')
 
+@app.route('/api/commits/')
+def get_commits():
+    try:
+        response = requests.get(GITHUB_API_URL)
+        if response.status_code != 200:
+            return jsonify({"error": "Impossible de récupérer les données"}), 500
+
+        commits = response.json()
+        commits_by_minute = {}
+
+        for commit in commits:
+            commit_date = commit['commit']['author']['date']
+            date_object = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ')
+            minute = date_object.strftime('%H:%M')  # Format HH:MM
+
+            commits_by_minute[minute] = commits_by_minute.get(minute, 0) + 1
+
+        return jsonify(commits_by_minute)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
